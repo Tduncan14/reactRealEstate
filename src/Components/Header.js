3d968b0a47aa5a723cc2f09676d4983b3ed02d9d
Treek <nbatre1@yahoo.com>
2019-10-15 18:03:29 -0500

    keep moving forward software engineer

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const Header = () => {
+
+
+
+
+  return(
+     <>
+    <h1> Real Estate App</h1>
+
+     </>
+  )
+}
+
+
+export default Header;
---
+++ src/Styles/StyledHeader.js
@@ -0,0 +1,7 @@
+import {Styled} from 'styled-components';
+
+
+export const Header = Styled.div `
+
+
+`;
