ee8ee110e8872634ef6b9342c15748256ca770a4
Treek <nbatre1@yahoo.com>
2019-10-20 19:58:52 -0500

    keep moving forward:

--- src/Components/Listing.js
+++ src/Components/Listing.js
@@ -8,13 +8,42 @@ const Listing = () =>{
          <section className="searchArea">Choose a home </section>
          
          <input type="text" name="search"/>
-         
+
          <section className="sortBy">
              
+             <div>
+                 390 results found
+             </div>
+
+             <div className="sortOptions">
+                 <select name="sortby" className="sortArea">
+                   <option value="price-asc">Highest Price</option>
+                   <option value="price-dsc">Lowest Price</option>
+                 </select>
+
+                  <div className="view">
+                  <i className="fa fa-th-list">📑</i>
+                  <i className="fa fa-th-menu">🗄</i>
+                  </div>
+             </div>
          </section>
 
          <section className="listingsResults">
-         
+            <div className ="listing">
+                <div className="listing-image">
+                    <span className="Address">Address</span>
+                    <div className="details">
+                        <div className="user-image">
+                            <img src=""/>
+                        </div>
+                        <div className="user-details">
+                            <span className="user-name">Will Smith</span>
+                        </div>
+                    </div>
+                    <img src="" />
+
+                </div>
+            </div>
          </section>
 
          <section className="pagination">
